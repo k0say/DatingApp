86d8bb3ff298c9ae0cede493cfced3e87e62f733
Giovanni Vittore <giovanni.vittore@alten.it>
2021-02-11 16:21:39 +0100

    Adding bootstrap via ng

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'client';
+
+  users: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getUsers()
+  }
+
+  getUsers() {
+    this.http.get('http://localhost:5000/api/users').subscribe(resp => {
+      this.users = resp;
+      console.log(resp);
+    },
+    error =>  {
+      console.log(error);
+    })
+  }
+
 }
